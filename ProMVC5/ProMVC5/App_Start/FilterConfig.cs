9c63169c7359cdb0d36d9329d60a3fb9c432578d
王宁 <wangning@zy-cast.com>
2020-05-21 23:03:00 +0800

    Initialize project

---
+++ ProMVC5/ProMVC5/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace ProMVC5
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
