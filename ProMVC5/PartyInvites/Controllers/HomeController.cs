f32cbc56d628d2c33d4ce3e6f6a0b90815c52bc3
王宁 <wangning@zy-cast.com>
2020-05-23 21:15:33 +0800

    传递动态输出

--- ProMVC5/PartyInvites/Controllers/HomeController.cs
+++ ProMVC5/PartyInvites/Controllers/HomeController.cs
@@ -11,6 +11,10 @@ namespace PartyInvites.Controllers
         // GET: Home
         public ViewResult Index()
         {
+            DateTime dt = DateTime.Now;
+            ViewBag.Time = dt.ToString($"现在是北京时间{dt.Year}年{dt.Month}月{dt.Day}日{dt.Hour}点{dt.Minute}分，" +
+                $"星期{(int)dt.DayOfWeek}。");
+            ViewBag.Greeting = dt.Hour < 12 ? "早上好！" : "下午好！";
             return View();
         }
 
