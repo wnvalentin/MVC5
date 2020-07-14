59dc47ccab38c5df6af04c10847fe4f8c2c09343
wnvalentin <bonjourwn@163.com>
2020-07-14 21:51:54 +0800

    添加bundles

---
+++ ClientFeatures/ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using ClientFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult MakeBooking()
+        {
+            return View(new Appointment
+            {
+                ClientName = "Adam",
+                TermsAccepted = true
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt)
+        {
+            // statements to store new Appointment in a
+            // repository would go here in a real project
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ ClientFeatures/ClientFeatures/Models/Appointment.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ClientFeatures.Models
+{
+    public class Appointment
+    {
+        [Required]
+        public string ClientName { get; set; }
+        public bool TermsAccepted { get; set; }
+    }
+}
